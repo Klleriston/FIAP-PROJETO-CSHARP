341f01856ec9129898e27ae8d871b74f6104f810
klleriston <klleandrade@outlook.com>
2024-11-11 22:10:24 -0300

    feat: add jwt settings

--- FIAP-PROJETO-CSHARP/Program.cs
+++ FIAP-PROJETO-CSHARP/Program.cs
@@ -1,12 +1,28 @@
 using FIAP_PROJETO_CSHARP.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseOracle(builder.Configuration.GetConnectionString("StringC")));
+
+var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]);
+
+builder.Services.AddAuthentication("CookieAuth")
+    .AddCookie("CookieAuth", options =>
+    {
+        options.Cookie.Name = "UserLoginCookie";
+        options.LoginPath = "/Account/Login";
+        options.LogoutPath = "/Account/Logout";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+        options.ExpireTimeSpan = TimeSpan.FromHours(2);
+        options.SlidingExpiration = true;
+    });
+
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
@@ -21,7 +37,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
